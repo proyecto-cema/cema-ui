c3f0812421578abec02bf427e253ff14896e5ff4
Agustin Suso <agustinsuso96@gmail.com>
2021-06-12 17:46:42 +0200

    'login'

---
+++ Pages/Principal.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace cema_ui.Pages
+{
+    public class login : PageModel
+    {
+        private readonly ILogger<login> _logger;
+
+        public login(ILogger<login> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
