52a2d88b4c0af9db0e88d9bd094f27b83e8ba97a
Francisco de Maussion <franciscodemaussion@gmail.com>
2021-08-10 19:03:03 -0300

    adds register try

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from 'vuex'
 import router from '../router'
-import {BASE_URL, LOGIN_PORT} from '../constants'
+import {BASE_URL, BOVINE_PORT, LOGIN_PORT} from '../constants'
 
 export default createStore({
   state: {
@@ -37,7 +37,15 @@ export default createStore({
     },
     async logUserIn({commit}, userData){
       try {
-        const res = await fetch("http://"+BASE_URL+":"+LOGIN_PORT+"/v1/users/login/"+userData.email+"?password="+userData.password);
+        const res = await fetch(BASE_URL+":"+LOGIN_PORT+"/v1/users/login/"+userData.email+"?password="+userData.password,
+        {
+          method: 'GET', // *GET, POST, PUT, DELETE, etc.
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic Y2VtYTpzbGlwa25vdA==',
+          },
+        });
         const userDB = await res.json();
         console.log('Usuario:', userDB);
         console.log(res)
@@ -49,6 +57,26 @@ export default createStore({
         commit('setError', null);
         localStorage.setItem('usuario', JSON.stringify(userDB))
         router.push('/');
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async registerUser({commit}, email, pass, userData){
+      try {
+        const res = await fetch(BASE_URL+":"+LOGIN_PORT+"/v1/users/login/"+email+"?password=12345",
+            {
+              method: 'PUT', // *GET, POST, PUT, DELETE, etc.
+              mode: 'cors',
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic Y2VtYTpzbGlwa25vdA==',
+              },
+              body: JSON.stringify(userData)
+            }
+          );
+        const helper = await res.json();
+        console.log('Bovine:', helper);
+        console.log(res)
       } catch (error) {
         console.error(error);
       }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,47 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form @submit.prevent="procesarFormulario">
+      <input
+          type="text"
+          placeholder="Usuario"
+          class="form-control my-2"
+      >
+    </form>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import {mapActions} from "vuex";
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
-  }
+  },
+  methods: {
+    ...mapActions(["registerUser"]),
+    async procesarFormulario() {
+      try {
+        await this.registerUser("pruebasuper@gmail.com", "1231412",{
+          "email": "pruebasuper@gmail.com",
+          "lastName": "super",
+          "name": "prueba",
+          "phone": 3541330188,
+          "role": "admin",
+          "userName": "prueba"
+        });
+        if (this.error !== null) {
+          return;
+        }
+        this.email = "";
+        this.pass1 = "";
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
 }
 </script>
