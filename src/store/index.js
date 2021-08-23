da8560fa4642b4cebe0c041ab6b7abe5bf7b793f
Agustin Suso <agustinsuso96@gmail.com>
2021-08-22 21:00:34 -0300

    .env

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import { createStore } from 'vuex'
 import auth from "./modules/auth.module";
 import mutations from "./mutations";
 import actions from "./actions";
+import bovine from "./modules/bovine.module";
 
 
 export default createStore({
   mutations: mutations,
   actions: actions,
   modules: {
-    auth
+    auth,
+    bovine
   }
 })
---
+++ src/store/modules/bovine.module.js
@@ -0,0 +1,77 @@
+import AuthService from '../../services/auth/auth.service';
+
+const user = JSON.parse(localStorage.getItem('user'));
+const initialState = user
+    ? { status: { loggedIn: true }, user }
+    : { status: { loggedIn: false }, user: null };
+
+
+const state = {
+    ...initialState,
+    error: {type: null, message: null}
+}
+
+const mutations = {
+    loginSuccess(state, user) {
+        state.status.loggedIn = true;
+        state.user = user;
+    },
+    loginFailure(state) {
+        state.status.loggedIn = false;
+        state.user = null;
+    },
+    logout(state) {
+        state.status.loggedIn = false;
+        state.user = null;
+    },
+    setError(state, payload){
+        if(payload === null) {
+            return state.error = {type: null, message: null}
+        }
+        if(payload === "Unauthorized"){
+            return state.error = {type: 'password', message: 'Contraseña incorrecta'}
+        }
+        if(payload === "Not Found"){
+            return state.error = {type: 'UserPass', message: 'Usuario o Contraseña incorrectos'}
+        }
+        if(payload === "Server"){
+            return state.error = {type: 'Server', message: 'ERROR en el servidor, intente nuevamente mas tarde.'}
+        }
+        return state.error = {type: 'Undefained', message: 'ERROR indefinido, intente nuevamente mas tarde.'}
+    }
+}
+
+const actions = {
+    login({ commit }, user) {
+        return AuthService.login(user).then(
+            user => {
+                commit('loginSuccess', user);
+                return Promise.resolve(user);
+            },
+            error => {
+                if(error.response.status === 404 || error.response.status === 401){
+                    console.log(error.response.statusText);
+                    commit('setError', error.response.statusText);
+                }
+                commit('loginFailure');
+                console.log(error);
+                return Promise.reject(error);
+            }
+        );
+    },
+    logout({ commit }) {
+        AuthService.logout();
+        commit('logout');
+    },
+}
+
+const getters = {
+}
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
+}
