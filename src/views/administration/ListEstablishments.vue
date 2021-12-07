090e83d6ff73401922072353d14bb160b1fd7ffd
Francisco de Maussion <franciscodemaussion@gmail.com>
2021-12-07 19:02:07 -0300

    fixes bugs in CM-226

--- src/views/administration/ListEstablishments.vue
+++ src/views/administration/ListEstablishments.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="text-center">
     <div class="d-grid gap-2 d-md-flex justify-content-md-end mb-2 mt-3">
-      <button class="btn btn-secondary text-white" type="button" @click="openAddEstablishmentModal(null)">
-        Nuevo Establecimiento
+      <button class="btn btn-secondary text-white" type="button"
+              v-on:click="openAddEstablishmentModal(null)">
+        + Nuevo Establecimiento
       </button>
     </div>
     <div class="mt-2">
