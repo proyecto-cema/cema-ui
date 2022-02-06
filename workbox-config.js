43f1cf4d258aa42b00288caedd9f18fc0f78db10
Francisco de Maussion <franciscodemaussion@gmail.com>
2022-02-06 20:37:21 -0300

    precaches images

--- workbox-config.js
+++ workbox-config.js
@@ -3,5 +3,15 @@ module.exports = {
   globPatterns: ["**/*.{css,ico,html,png,js,json,woff2}"],
   swDest: "./public/service-worker.js",
   skipWaiting: true,
-  clientsClaim: true
+  clientsClaim: true,
+  runtimeCaching: [{
+    urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+    handler: 'CacheFirst',
+    options: {
+      cacheName: 'CemaImages',
+      expiration: {
+        maxEntries: 5,
+      },
+    },
+  }],
 };
