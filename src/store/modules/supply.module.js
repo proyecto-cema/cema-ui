7a822cafdd9b2a1890298b581d47646624cd8891
Francisco de Maussion <franciscodemaussion@gmail.com>
2022-02-28 12:37:32 -0300

    fixes supply search

--- src/store/modules/supply.module.js
+++ src/store/modules/supply.module.js
@@ -65,8 +65,8 @@ const actions = {
         );
     },
     async listSupplies({dispatch, rootState}, data) {
-        data.search={"establishmentCuig":rootState.auth.user.user.establishmentCuig}
-        console.log("DATASEARCH: "+data.search)
+        data.search["establishmentCuig"] = rootState.auth.user.user.establishmentCuig
+        console.log("DATASEARCH: ", data.search)
         return SupplyService.getSuppliesList(data.page, data.size, data.search).then(
             response => {
                 console.log(response.data);
